f33eba53285ffa9e345ac900341d083dfb672113
The-Mizza <dirtymeff@gmail.com>
2023-10-13 00:30:55 +0300

    update

--- project/app/routers.py
+++ project/app/routers.py
@@ -6,10 +6,23 @@ from fastapi import Depends, APIRouter
 router = APIRouter()
 
 
+async def get_another_and_create(database):
+    while True:
+        try:
+            another_questions = await outer_services.jservice_request(1)
+            db_services.create_question(
+                schemas.QuestionSchema(question=another_questions[0]["question"],
+                                       answer=another_questions[0]["answer"]),
+                database
+            )
+            break
+        except:
+            pass
+
 @router.post('/')
 async def add_questions(data: schemas.RequestDataSchema = None, database: Session = Depends(get_db)):
     data_to_return = db_services.get_last_question(database)
-    if data_to_return is None:
+    if not data_to_return:
         returned_question = {}
     else:
         returned_question = schemas.QuestionSchema(
@@ -19,27 +32,16 @@ async def add_questions(data: schemas.RequestDataSchema = None, database: Sessio
             created_at=data_to_return.created_at
         )
     questions = await outer_services.jservice_request(data.questions_num)
-
     for question in questions:
         try:
             create_data = schemas.QuestionSchema(question=question["question"], answer=question["answer"])
             db_services.create_question(create_data, database)
         except:
-            while True:
-                try:
-                    another_questions = await outer_services.jservice_request(1)
-                    db_services.create_question(
-                        schemas.QuestionSchema(question=another_questions[0]["question"],
-                                               answer=another_questions[0]["answer"]),
-                        database
-                    )
-                    break
-                except:
-                    pass
-
+            await get_another_and_create(database)
     return returned_question
 
 
-@router.get('/{id}')
+
+@router.get('/')
 async def get_question(question_id: int = None, database: Session = Depends(get_db)):
     return db_services.get_question(question_id, database)
